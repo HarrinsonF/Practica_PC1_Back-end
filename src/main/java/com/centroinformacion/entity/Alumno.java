1b6e7b988eaab6e78ed28dd27c7d8a0830538e21
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2023-10-14 21:42:15 -0500

    ajustes

--- src/main/java/com/centroinformacion/entity/Alumno.java
+++ src/main/java/com/centroinformacion/entity/Alumno.java
@@ -4,6 +4,9 @@ import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -14,21 +17,13 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "alumno")
-@AllArgsConstructor
-@NoArgsConstructor
 public class Alumno {
   
 
